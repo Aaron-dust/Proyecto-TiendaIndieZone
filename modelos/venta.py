e56683d59c17deebced2e5de6868e406c5336f3d
Mariano Rodas <mariano.rodasr@istpargentina.edu.pe>
2026-08-11 11:02:59 -0500

    commit 35: Actualización del modelo de ventas

--- modelos/venta.py
+++ modelos/venta.py
@@ -5,50 +5,39 @@
 # ----------------------------------------------------------------------------------
 
 class Venta:
-
-    def __init__(self, fecha_venta, total_venta, id_cliente):
-
-        self.id = None
+    def __init__(
+        self,
+        fecha_venta,
+        total_venta,
+        id_cliente
+    ):
+        self.id_venta = None
         self.fecha_venta = fecha_venta
         self.total_venta = total_venta
         self.id_cliente = id_cliente
 
     def __str__(self):
-
         return (
-
-            f"[{self.id}] "
-
+            f"[{self.id_venta}] "
             f"Cliente: {self.id_cliente} | "
-
-            f"S/. {self.total_venta:.2f} | "
-
-            f"{self.fecha_venta}"
-
+            f"Total: S/. {self.total_venta} | "
+            f"Fecha: {self.fecha_venta}"
         )
 
     def to_dict(self):
-
         return {
-
-            "id": self.id,
+            "id_venta": self.id_venta,
             "fecha_venta": self.fecha_venta,
             "total_venta": self.total_venta,
             "id_cliente": self.id_cliente
-
         }
 
     @classmethod
     def from_dict(cls, datos):
-
         venta = cls(
-
             datos["fecha_venta"],
             datos["total_venta"],
             datos["id_cliente"]
-
         )
-
-        venta.id = datos["id"]
-
+        venta.id_venta = datos.get("id_venta")
         return venta
