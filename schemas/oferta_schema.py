bce3baab3db7a4c9fd22d0f0b81963f5ed7031fc
Mariano Rodas <mariano.rodasr@istpargentina.edu.pe>
2026-08-11 09:48:23 -0500

    Commit 44: Corrección de gestión de ofertas

--- schemas/oferta_schema.py
+++ schemas/oferta_schema.py
@@ -1,39 +1,22 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel
 from typing import Optional
-
+from datetime import date
 
 class OfertaCrear(BaseModel):
     nombre: str
     porcentaje_descuento: float
-    fecha_inicio: str
-    fecha_fin: str
-    @field_validator("porcentaje_descuento")
-    @classmethod
-    def validar_descuento(cls, valor):
-        if valor < 0 or valor > 100:
-            raise ValueError(
-                "El porcentaje de descuento debe estar entre 0 y 100"
-            )
-        return valor
+    fecha_inicio: date
+    fecha_fin: date
 
 class OfertaActualizar(BaseModel):
     nombre: Optional[str] = None
     porcentaje_descuento: Optional[float] = None
-    fecha_inicio: Optional[str] = None
-    fecha_fin: Optional[str] = None
-
-    @field_validator("porcentaje_descuento")
-    @classmethod
-    def validar_descuento(cls, valor):
-        if valor is not None and (valor < 0 or valor > 100):
-            raise ValueError(
-                "El porcentaje de descuento debe estar entre 0 y 100"
-            )
-        return valor
+    fecha_inicio: Optional[date] = None
+    fecha_fin: Optional[date] = None
 
 class OfertaRespuesta(BaseModel):
     id_oferta: int
     nombre: str
     porcentaje_descuento: float
-    fecha_inicio: str
-    fecha_fin: str
+    fecha_inicio: date
+    fecha_fin: date
