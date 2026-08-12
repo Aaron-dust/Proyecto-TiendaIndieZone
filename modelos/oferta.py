ac46200576ac8d9c509cf2f03dfcd3d1b3e7ba9a
Mariano Rodas <mariano.rodasr@istpargentina.edu.pe>
2026-08-12 04:39:08 -0500

    Commit 45: Corrección de gestión de ofertas

--- modelos/oferta.py
+++ modelos/oferta.py
@@ -1,11 +1,6 @@
-# ----------------------------------------------------------------------------------
-# MODELO – Oferta
-#
-# Clase que representa una oferta registrada en el sistema.
-# ----------------------------------------------------------------------------------
+# modelos/oferta.py
 
 class Oferta:
-
     def __init__(
         self,
         nombre,
@@ -13,39 +8,18 @@ class Oferta:
         fecha_inicio,
         fecha_fin
     ):
-        self.id_oferta = None
+        self.id = None
         self.nombre = nombre
         self.porcentaje_descuento = porcentaje_descuento
         self.fecha_inicio = fecha_inicio
         self.fecha_fin = fecha_fin
 
-    def __str__(self):
-        return (
-            f"[{self.id_oferta}] "
-            f"{self.nombre} | "
-            f"Descuento: {self.porcentaje_descuento}% | "
-            f"Inicio: {self.fecha_inicio} | "
-            f"Fin: {self.fecha_fin}"
-        )
-
     def to_dict(self):
         return {
-            "id_oferta": self.id_oferta,
+            "id_oferta": self.id,
             "nombre": self.nombre,
-            "porcentaje_descuento": self.porcentaje_descuento,
+            "porcentaje_descuento":
+                float(self.porcentaje_descuento),
             "fecha_inicio": self.fecha_inicio,
             "fecha_fin": self.fecha_fin
         }
-
-    @classmethod
-    def from_dict(cls, datos):
-        oferta = cls(
-            datos["nombre"],
-            datos["porcentaje_descuento"],
-            datos["fecha_inicio"],
-            datos["fecha_fin"]
-        )
-
-        oferta.id_oferta = datos.get("id_oferta")
-
-        return oferta
