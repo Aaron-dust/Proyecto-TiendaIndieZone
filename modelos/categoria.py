4ef702cbc4c2c8ae5917a20dcb8743d060d95163
Mariano Rodas <mariano.rodasr@istpargentina.edu.pe>
2026-08-12 04:35:54 -0500

    Commit 36: Arreglando el modificado de categoría modelo
    Updated the Categoria class to remove unused methods and change attribute names.

--- modelos/categoria.py
+++ modelos/categoria.py
@@ -1,38 +1,14 @@
-# ----------------------------------------------------------------------------------
-# MODELO – Categoria
-# 
-# Clase que representa una categoría de productos.
-# Solo almacena la información; la gestión la realiza el DAO.
-# ----------------------------------------------------------------------------------
+# modelos/categoria.py
 
 class Categoria:
-    def __init__(self, nombre, descripcion=None):
-        self.id_categoria = None
+    def __init__(self, nombre, descripcion):
+        self.id = None
         self.nombre = nombre
         self.descripcion = descripcion
 
-    def __str__(self):
-        return (
-            f"[{self.id_categoria}] "
-            f"{self.nombre} | "
-            f"{self.descripcion}"
-        )
-        
-    # Convierte el objeto a diccionario
     def to_dict(self):
         return {
-            "id_categoria": self.id_categoria,
+            "id_categoria": self.id,
             "nombre": self.nombre,
             "descripcion": self.descripcion
         }
-
-    # Crea un objeto Categoria desde un diccionario
-    @classmethod
-    def from_dict(cls, datos):
-
-        categoria = cls(
-            datos["nombre"],
-            datos.get("descripcion")
-        )
-        categoria.id_categoria = datos.get("id_categoria")
-        return categoria
