78c75522411926cb8daa966695d75b35c0174ba1
Mariano Rodas <mariano.rodasr@istpargentina.edu.pe>
2026-08-12 04:41:39 -0500

    Commit 41: Configurando schemas categoria

--- schemas/categoria_schema.py
+++ schemas/categoria_schema.py
@@ -1,16 +1,15 @@
-from pydantic import BaseModel
-from typing import Optional
+# schemas/categoria_schema.py
 
+from typing import Optional
+from pydantic import BaseModel
 
 class CategoriaCrear(BaseModel):
     nombre: str
     descripcion: Optional[str] = None
 
-
 class CategoriaActualizar(BaseModel):
     nombre: Optional[str] = None
     descripcion: Optional[str] = None
-
 
 class CategoriaRespuesta(BaseModel):
     id_categoria: int
