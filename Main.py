ee80264a5c019cedf1cf94f8a81b8ecde82be90f
Mariano Rodas <mariano.rodasr@istpargentina.edu.pe>
2026-08-12 04:46:34 -0500

    commit 44: Código terminado con las correcciones correspondientes
    Updated the title and description of the FastAPI application. Modified CORS settings to include additional origins and removed the 'registros' router.

--- Main.py
+++ Main.py
@@ -1,46 +1,60 @@
+# main.py
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from config.base_datos import inicializar
+
 from routers import (
     clientes,
     categorias,
     ofertas,
     productos,
     ventas,
-    detalle_ventas,
-    registros
+    detalle_ventas
 )
 
 app = FastAPI(
-    title="Sistema de Gestión de TiendaIndieZone",
+    title="Sistema de Gestión TiendaIndieZone",
     version="1.0",
-    description="API REST para la gestión de clientes, categorías, ofertas, productos y ventas"
+    description="API para gestionar la tienda"
 )
 
+# Permite que React se conecte con FastAPI.
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:3000"
+        "http://127.0.0.1:5173",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
     ],
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
+# Revisa la conexión con PostgreSQL.
 inicializar()
 
+# Se agregan las rutas del sistema.
 app.include_router(clientes.router)
+
 app.include_router(categorias.router)
+
 app.include_router(ofertas.router)
+
 app.include_router(productos.router)
+
 app.include_router(ventas.router)
+
 app.include_router(detalle_ventas.router)
-app.include_router(registros.router)
 
 @app.get("/")
 def inicio():
     return {
-        "mensaje": "API Sistema de Gestión de TiendaIndieZone",
+        "mensaje":
+            "API Sistema de Gestión TiendaIndieZone",
         "version": "1.0",
         "docs": "/docs"
     }
